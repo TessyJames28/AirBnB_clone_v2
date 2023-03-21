ee49ca4e575ff21fddb930af4f32284db9235984
TessyJames28 <tessyjames28@gmail.com>
2023-03-21 18:25:14 +0100

    modified the state module for sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(City, cascade="all, delete-orphan", backref="state")
+
+    @property
+    def cities(self):
+        """getter attribute that returns the list of City instances"""
+        city_vals = storage.all(City)
+        city_lists = []
+
+        for city in city_vals:
+            if City.state_id == self.id:
+                city_list.append(city)
+
+        return city_list
