cf382c0523ce89d2e6f89ffa697afa0c2cc47c86
TessyJames28 <tessyjames28@gmail.com>
2023-03-21 05:22:16 +0100

    create database and grant privileges to new user

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,5 @@
--- Script that create and grant privileges to user hbnb_test --
+-- Script that create database and grant privileges to user hbnb_test --
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'; 
