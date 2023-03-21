5362de8d155b96d5319e6b5898c4d6c8d99cc1e1
TessyJames28 <tessyjames28@gmail.com>
2023-03-21 04:30:42 +0100

    create and grant user privilege

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+-- Script that create and grant privileges to user hbnb_dev --
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; 
