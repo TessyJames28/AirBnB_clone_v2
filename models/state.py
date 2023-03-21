68fd5cf1c80115ab3a40eeea6c860297f083390b
muideen7 <olayeyeayomide2000@gmail.com>
2023-03-21 20:19:22 +0100

    updated state module for sqlachemy

--- models/state.py
+++ models/state.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(City, cascade="all, delete-orphan", backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
-        """getter attribute that returns the list of City instances"""
-        city_vals = storage.all(City)
-        city_lists = []
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
 
-        for city in city_vals:
-            if City.state_id == self.id:
-                city_list.append(city)
-
-        return city_list
