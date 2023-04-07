e068322b2270a5950815818b62efa4d7e67be691
TessyJames28 <tessyjames28@gmail.com>
2023-04-07 02:10:33 +0100

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -22,6 +22,7 @@ def do_pack():
     except Exception:
         return None
 
+
 def do_deploy(archive_path):
     """fabric command to deploy archive to remote servers"""
     if os.path.isfile(archive_path) is False:
@@ -47,6 +48,7 @@ def do_deploy(archive_path):
         return True
     except Exception:
         return False
+
 
 def deploy():
     """creates and distributes an archive to your web servers"""
