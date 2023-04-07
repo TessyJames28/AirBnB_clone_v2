bf1965012b89840b4e23f89717366f0bf033b418
Tessyjames <tessyjames28@gmail.com>
2023-04-07 10:08:54 +0000

    deleting old archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fabric script that deletes out of date archives"""
-from fabric.api import run, env, cd, lcd 
+from fabric.api import run, env, cd, lcd, local
 import os
 
 
@@ -9,16 +9,15 @@ env.hosts = ["100.25.119.211", "100.25.48.208"]
 
 def do_clean(number=0):
     """Deletes out of date archives and leaves most recent ones"""
-    number = 1 if int(number) <= 1 else int(number)
+    num = 1 if int(number) <= 1 else int(number)
  
     archives = sorted(os.listdir("versions"))
     with lcd("versions"):
-        for archive in archives:
-            run("rm -f {}".format(archive))
+        for archive in archives[:-num]:
+            local("rm -f ./{}".format(archive))
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
-        archives = [arch for arch in archives if "web_static_" in arch]
-        del_arch = archives[:-number]
+        del_arch = archives[:-num]
         for archive in del_arch:
-            run("rm -f {}".format(archive))
+            run("rm -f ./{}".format(archive))
