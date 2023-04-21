b4dd52c7b9e1d1a885ad85b4703e333661a8d86e
TessyJames28 <tessyjames28@gmail.com>
2023-04-21 22:45:40 +0100

    updating file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,7 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """deserialising the JSON file objects"""
+        self.reload()
