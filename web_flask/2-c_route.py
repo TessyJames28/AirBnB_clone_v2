ef754fb9c2b774354939a9be0ecab1da2a4ffcfa
TessyJames28 <tessyjames28@gmail.com>
2023-04-21 20:04:22 +0100

    use variable in flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+Requirement:
+    web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """function that displays hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """function that returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """function that accepts variable"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
